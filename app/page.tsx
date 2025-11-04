debc97f60e3f269555d1e2e692b2bd649ea452ea
Rubén Giménez Martínez <rgimenez.etraid@grupoetra.com>
2025-11-04 14:12:55 +0100

    Quitar el logo de next.js

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@ export default function Home() {
   return (
     <div>
       <h1>Hola mundo desde Next.js App</h1>
-      <Image
-        className="dark:invert"
-        src="/next.svg"
-        alt="Next.js logo"
-        width={100}
-        height={20}
-        priority
-      />
     </div>
   );
 }
